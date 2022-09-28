e71f96ac4a40ba79d826226d5f569dd5d84b1098
json <wjs330183@163.com>
2022-09-28 14:26:50 +0800

    test

---
+++ gamma.py
@@ -0,0 +1,29 @@
+# gamma变换
+import cv2
+import numpy as np
+
+# 创建用于显示gamma的窗口和调节参数的bar
+cv2.namedWindow("config", cv2.WINDOW_NORMAL)
+cv2.moveWindow("left", 0, 0)
+cv2.moveWindow("right", 600, 0)
+
+cv2.createTrackbar("gamma", "config", 1, 10, lambda x: None)
+cv2.createTrackbar("gain", "config", 1, 10, lambda x: None)
+cv2.createTrackbar("scale", "config", 200, 255, lambda x: None)
+
+image_path = './1.jpg'
+img = cv2.imread(image_path)
+
+gamma = cv2.getTrackbarPos("gamma", "config")
+gain = cv2.getTrackbarPos("gain", "config")
+scale = cv2.getTrackbarPos("scale", "config")
+
+# gamma, gain, scale = 0.7, 1, 255
+gamma_img = np.zeros_like(img)
+for i in range(3):
+    gamma_img[:, :, i] = ((img[:, :, i] / scale) ** gamma) * scale * gain
+cv2.imshow('img', img)
+cv2.imshow('gamma_img', gamma_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
