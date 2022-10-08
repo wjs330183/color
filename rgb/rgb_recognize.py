de0fe5fc29cbf9c2c5811c43368e8a9ed9f7e470
json <wjs330183@163.com>
2022-10-08 10:14:58 +0800

    remove

--- rgb/rgb_recognize.py
+++ rgb/rgb_recognize.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
+from skimage_test import morphology
 
-pic_file = './../2.jpg'
+pic_file = './../1.jpg'
 
 img = cv2.imread(pic_file)
+
 b_img, g_img, r_img = cv2.split(img)
 cv2.imshow('B', b_img)
 
@@ -11,6 +13,7 @@ cv2.imshow('G', g_img)
 
 cv2.imshow('R', r_img)
 b_ret, b_binary = cv2.threshold(b_img, 200, 255, cv2.THRESH_BINARY)
+
 b_canny = cv2.Canny(b_binary, 50, 150)
 b_edge = img.copy()
 b_lines = cv2.HoughLinesP(b_canny, 1, np.pi / 180, 10, minLineLength=100, maxLineGap=50)
@@ -25,6 +28,7 @@ cv2.imshow("b_canny", b_canny)
 cv2.imshow("b_edge", b_edge)
 
 g_ret, g_binary = cv2.threshold(g_img, 200, 255, cv2.THRESH_BINARY)
+
 g_canny = cv2.Canny(g_binary, 50, 150)
 g_edge = img.copy()
 g_lines = cv2.HoughLinesP(g_canny, 1, np.pi / 180, 10, minLineLength=100, maxLineGap=50)
@@ -40,13 +44,16 @@ cv2.imshow("g_canny", g_canny)
 cv2.imshow("g_edge", g_edge)
 
 r_ret, r_binary = cv2.threshold(r_img, 200, 255, cv2.THRESH_BINARY)
+r_binary = cv2.medianBlur(r_binary, 5)
+r_binary = morphology.remove_small_objects(r_binary)
+# r_binary = morphology.remove_small_holes(r_binary).astype(np.uint8)
 r_canny = cv2.Canny(r_binary, 50, 150)
 r_edge = img.copy()
 r_lines = cv2.HoughLinesP(r_canny, 1, np.pi / 180, 10, minLineLength=100, maxLineGap=50)
-for line in r_lines:
-    x1, y1, x2, y2 = line[0]
-    if abs(y1 - y2) < 20:
-        cv2.line(r_edge, (x1, y1), (x2, y2), (0, 0, 255), 3)
+# for line in r_lines:
+#     x1, y1, x2, y2 = line[0]
+#     if abs(y1 - y2) < 20:
+#         cv2.line(r_edge, (x1, y1), (x2, y2), (0, 0, 255), 3)
 
 cv2.imshow("r_ret", r_ret)
 cv2.imshow("r_binary", r_binary)
--- rgb/rgb_test.py
+++ rgb/rgb_test.py
@@ -1,9 +1,23 @@
 import cv2
 import numpy as np
 
-pic_file = './../3.jpg'
-img = cv2.imread(pic_file)
+pic_file = './../colors.jpeg'
+img = cv2.imread(pic_file, cv2.IMREAD_COLOR)
+
 b, g, r = cv2.split(img)
+
+whiteLower = np.array([150, 150, 150])
+whiteUpper = np.array([255, 255, 255])
+white_mask = cv2.inRange(img, whiteLower, whiteUpper)
+cv2.imshow('white_mask', white_mask)
+
+
+
+blueLower = np.array([255, 0, 0])
+blueUpper = np.array([255, 255, 200])
+blue_mask = cv2.inRange(img, blueLower, blueUpper)
+cv2.imshow('blue_mask', blue_mask)
+
 
 cv2.imshow('B', b)
 
